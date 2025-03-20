ceb5d9c7af31be3193c8d2727ac058a9e56e8403
Jetter <jlgarcia2@student.apc.edu.ph>
2025-03-20 14:18:46 +0800

    feat: add HeroSection component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
 import Header from "@/components/homepage/header";
+import HeroSection from "@/components/homepage/hero-section";
 
 export default function HomePage() {
   return (
     <div>
       <Header />
+      <main>
+        <HeroSection />
+      </main>
     </div>
   );
 }
