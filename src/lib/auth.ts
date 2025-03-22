36a7a414953011192219560c079e8df5e94ae893
Jetter <jlgarcia2@student.apc.edu.ph>
2025-03-23 07:06:14 +0800

    feat: add additional information in user session

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,26 @@
-import NextAuth from "next-auth";
+import NextAuth, { DefaultSession } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "@/lib/prisma";
 import Credentials from "next-auth/providers/credentials";
 import bcrypt from "bcryptjs";
+
+declare module "next-auth" {
+  interface User {
+    firstName?: string;
+    lastName?: string;
+    middleInitial?: string;
+    role?: string;
+  }
+
+  interface Session {
+    user: {
+      firstName?: string;
+      lastName?: string;
+      middleInitial?: string;
+      role?: string;
+    } & DefaultSession["user"];
+  }
+}
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
@@ -35,8 +53,37 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
           return null;
         }
 
-        return user;
+        return {
+          email: user.email,
+          role: user.role,
+          id: user.id,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          middleInitial: user.middleInitial || undefined,
+        };
       },
     }),
   ],
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.firstName = user.firstName;
+        token.middleInitial = user.middleInitial;
+        token.lastName = user.lastName;
+        token.role = user.role;
+      }
+      return token;
+    },
+    session({ session, token }) {
+      if (token && session.user) {
+        session.user.id = token.id as string;
+        session.user.firstName = token.firstName as string;
+        session.user.middleInitial = token.middleInitial as string;
+        session.user.lastName = token.lastName as string;
+        session.user.role = token.role as string | undefined;
+      }
+      return session;
+    },
+  },
 });
