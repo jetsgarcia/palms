6f9dba7069477dfb9ec697b07eabb47f74d46369
Jetter <jlgarcia2@student.apc.edu.ph>
2025-04-28 20:47:48 +0800

    fix: Refactor fetchFirstLogin to use environment variable for API base URL

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,11 +6,14 @@ const unprotectedRoutes = ["/login", "/", "/change-password"];
 
 async function fetchFirstLogin(userId: string) {
   try {
-    const response = await fetch("http://localhost:3000/api/first-login", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ userId }),
-    });
+    const response = await fetch(
+      `${process.env.APP_API_BASE_URL}/api/first-login`,
+      {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ userId }),
+      }
+    );
 
     if (!response.ok) throw new Error("Failed to fetch first login status");
 
