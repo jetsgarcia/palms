9be45f67b962d981f2abd43e04d65bba4dce0309
Jetter <jlgarcia2@student.apc.edu.ph>
2025-04-28 18:58:43 +0800

    feat: Add motion animations to ContactSection for enhanced user interaction

--- src/app/(routes)/(landing)/_components/contact-section.tsx
+++ src/app/(routes)/(landing)/_components/contact-section.tsx
@@ -1,24 +1,54 @@
+"use client";
+
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { motion } from "motion/react";
 
 export default function ContactSection() {
   return (
-    <section id="contact" className="w-full py-12 md:py-24 lg:py-40 bg-muted">
+    <section id="contact" className="w-full py-12 md:py-24 lg:py-40">
       <div className="container mx-auto px-4 md:px-6">
         <div className="grid gap-10 px-10 md:gap-16 lg:grid-cols-2">
-          <div className="space-y-4">
-            <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">
+          <div>
+            <motion.h2
+              initial={{ opacity: 0, x: -20 }}
+              whileInView={{
+                opacity: 1,
+                x: 0,
+                transition: { duration: 0.3, ease: "easeInOut" },
+              }}
+              exit={{ opacity: 0, x: -20 }}
+              className="text-3xl font-bold tracking-tighter mb-1 md:text-4xl/tight"
+            >
               Dedicated to the Philippine Army
-            </h2>
-            <p className="text-muted-foreground md:text-xl">
+            </motion.h2>
+            <motion.p
+              initial={{ opacity: 0, x: -20 }}
+              whileInView={{
+                opacity: 1,
+                x: 0,
+                transition: { duration: 1, ease: "easeInOut", delay: 0.3 },
+              }}
+              className="text-muted-foreground mb-4"
+            >
               Our platform is designed exclusively for the Philippine Army to
               provide secure access to training materials and resources. We are
               committed to supporting the professional development of our
               military personnel.
-            </p>
-            <Button asChild>
-              <Link href="/">Contact Support</Link>
-            </Button>
+            </motion.p>
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              whileInView={{
+                opacity: 1,
+                y: 0,
+                transition: { duration: 1, ease: "easeInOut", delay: 0.6 },
+              }}
+              exit={{ opacity: 0, y: 20 }}
+            >
+              <Button asChild>
+                <Link href="/">Contact Support</Link>
+              </Button>
+            </motion.div>
           </div>
 
           <div className="flex flex-col space-y-4">
