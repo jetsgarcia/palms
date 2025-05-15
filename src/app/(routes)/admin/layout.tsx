a064533f25ab12f0181d63a9ce267da2779b843e
Jetter <jlgarcia2@student.apc.edu.ph>
2025-05-15 12:33:47 +0800

    refactor: remove unnecessary blank line

--- src/app/(routes)/admin/layout.tsx
+++ src/app/(routes)/admin/layout.tsx
@@ -1,5 +1,4 @@
 import { cookies } from "next/headers";
-
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/app-sidebar";
 import { auth } from "@/lib/auth";
