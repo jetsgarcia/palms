4ad662eb5149d35ca2e63d3fd749456af70419c3
Jetter <jlgarcia2@student.apc.edu.ph>
2025-06-19 14:08:09 +0800

    refactor: update import paths for ChangePassword components and fetchFirstLogin action

--- src/app/(routes)/(auth)/change-password/page.tsx
+++ src/app/(routes)/(auth)/change-password/page.tsx
@@ -1,7 +1,7 @@
 import { auth } from "@/lib/auth";
-import ChangePasswordForm from "./_components/change-password-form";
-import ChangePasswordWithOTPForm from "./_components/change-password-with-otp-form";
-import { fetchFirstLogin } from "./_actions/fetch-first-login";
+import ChangePasswordForm from "@/components/change-password-form";
+import ChangePasswordWithOTPForm from "@/components/change-password-with-otp-form";
+import { fetchFirstLogin } from "@/actions/fetch-first-login";
 
 export default async function ChangePasswordPage() {
   const session = await auth();
