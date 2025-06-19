9e3770e68f47fbde9b9842a4318d1fa0d3e36f31
Jetter <jlgarcia2@student.apc.edu.ph>
2025-06-19 13:50:29 +0800

    feat: implement createAFOS server action

---
+++ src/app/(routes)/(roles)/admin/course-management/add-afos/actions/createAFOS.ts
@@ -0,0 +1,26 @@
+"use server";
+
+import { z } from "zod";
+import { prisma } from "@/lib/prisma";
+import { createAFOSFormSchema } from "@/schemas/createAFOSForm";
+
+export async function createAFOS(values: z.infer<typeof createAFOSFormSchema>) {
+  try {
+    await prisma.afos.create({
+      data: {
+        code: values.code,
+        name: values.name,
+        level: values.level,
+        trainingPeriod: {
+          connect: { id: values.trainingPeriodId },
+        },
+      },
+    });
+
+    return { success: true };
+  } catch (error) {
+    if (error instanceof Error) {
+      return { error: error.message };
+    }
+  }
+}
