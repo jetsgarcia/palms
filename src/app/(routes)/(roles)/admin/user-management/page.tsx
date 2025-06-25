f9e06305eb87a00b6bbb5acbdf22a6ba630dfe3d
Jetter <jlgarcia2@student.apc.edu.ph>
2025-06-25 18:08:28 +0800

    feat: add error handling

--- src/app/(routes)/(roles)/admin/user-management/page.tsx
+++ src/app/(routes)/(roles)/admin/user-management/page.tsx
@@ -1,17 +1,17 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { allUserColumns } from "../../../../../components/all-user-columns";
-import { AllUserDataTable } from "../../../../../components/all-user-data-table";
+import { allUserColumns } from "@/components/all-user-columns";
+import { AllUserDataTable } from "@/components/all-user-data-table";
 import { useEffect, useState } from "react";
-import { StudentsDataTable } from "../../../../../components/students-data-table";
-import { studentsColumns } from "../../../../../components/students-columns";
-import { InstructorsDataTable } from "../../../../../components/instructors-data-table";
-import { instructorsColumns } from "../../../../../components/instructors-columns";
-import { adminsColumns } from "../../../../../components/admins-columns";
-import { AdminsDataTable } from "../../../../../components/admins-data-table";
-import { Plus } from "lucide-react";
-import { fetchStudents, fetchUsers } from "../../../../../actions/fetchUsers";
+import { StudentsDataTable } from "@/components/students-data-table";
+import { studentsColumns } from "@/components/students-columns";
+import { InstructorsDataTable } from "@/components/instructors-data-table";
+import { instructorsColumns } from "@/components/instructors-columns";
+import { adminsColumns } from "@/components/admins-columns";
+import { AdminsDataTable } from "@/components/admins-data-table";
+import { CircleAlert, Plus } from "lucide-react";
+import { fetchStudents, fetchUsers } from "@/actions/fetchUsers";
 import { UserType } from "@/types/user";
 import { StudentType } from "@/types/student";
 import {
@@ -29,190 +29,214 @@ export default function UserManagementPage() {
   const [allUsersData, setAllUsersData] = useState<UserType[]>([]);
   const [studentsData, setStudentsData] = useState<StudentType[]>([]);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
   const [active, setActive] = useState("all");
   const router = useRouter();
 
   useEffect(() => {
     async function loadData() {
-      setLoading(true);
-      const [users, students] = await Promise.all([
-        fetchUsers(),
-        fetchStudents(),
-      ]);
+      try {
+        setLoading(true);
+        const [users, students] = await Promise.all([
+          fetchUsers(),
+          fetchStudents(),
+        ]);
 
-      setAllUsersData(users || []);
-      const safeData: StudentType[] = (students ?? []).filter(
-        (item): item is StudentType => item.student !== null
-      );
-      setStudentsData(safeData);
-      setLoading(false);
+        setAllUsersData(users || []);
+        const safeData: StudentType[] = (students ?? []).filter(
+          (item): item is StudentType => item.student !== null
+        );
+        setStudentsData(safeData);
+      } catch (error) {
+        setError(error instanceof Error ? error.message : "An error occurred");
+      } finally {
+        setLoading(false);
+      }
     }
 
     loadData();
   }, []);
 
   return (
-    <div>
-      <div className="flex items-center justify-between">
-        <div className="flex items-center">
-          <Button
-            variant={active === "all" ? "default" : "ghost"}
-            onClick={() => {
-              setActive("all");
-            }}
-          >
-            All
-          </Button>
-          <Button
-            variant={active === "students" ? "default" : "ghost"}
-            onClick={() => {
-              setActive("students");
-            }}
-          >
-            Students
-          </Button>
-          <Button
-            variant={active === "instructors" ? "default" : "ghost"}
-            onClick={() => {
-              setActive("instructors");
-            }}
-          >
-            Instructors
-          </Button>
-          <Button
-            variant={active === "admins" ? "default" : "ghost"}
-            onClick={() => {
-              setActive("admins");
-            }}
-          >
-            Admins
-          </Button>
+    <>
+      {error ? (
+        <div className="my-4 flex items-center justify-center h-[calc(100dvh-7rem)]">
+          <div className="bg-red-100 border border-red-400 text-red-700 px-6 py-4 rounded-lg shadow-md flex items-center gap-2 animate-fade-in">
+            <CircleAlert />
+            <span className="font-medium">{error}</span>
+          </div>
         </div>
-        {active === "all" ? (
-          <Dialog>
-            <DialogTrigger asChild>
-              <Button>
-                <Plus /> Add User
-              </Button>
-            </DialogTrigger>
-            <DialogContent className="sm:max-w-[425px]">
-              <DialogHeader>
-                <DialogTitle>Select User</DialogTitle>
-                <DialogDescription>
-                  Please select the user you want to register.
-                </DialogDescription>
-              </DialogHeader>
-              <div className="grid gap-4 py-4">
-                <Button
-                  variant="outline"
-                  className="hover:bg-primary hover:text-white"
-                >
-                  Student
-                </Button>
-                <Button
-                  variant="outline"
-                  className="hover:bg-primary hover:text-white"
-                  onClick={() => {
-                    router.push("/admin/user-management/add-instructor");
-                  }}
-                >
-                  Instructor
-                </Button>
-                <Button
-                  variant="outline"
-                  className="hover:bg-primary hover:text-white"
-                  onClick={() => {
-                    router.push("/admin/user-management/add-admin");
-                  }}
-                >
-                  Admin
-                </Button>
-              </div>
-            </DialogContent>
-          </Dialog>
-        ) : (
-          <Button
-            onClick={() => {
-              if (active === "admins") {
-                router.push("/admin/user-management/add-admin");
-              }
-
-              if (active === "instructors") {
-                router.push("/admin/user-management/add-instructor");
-              }
-            }}
-          >
-            <Plus />
-            Add
-            {active === "students" && " Student"}
-            {active === "instructors" && " Instructor"}
-            {active === "admins" && " Admin"}
-          </Button>
-        )}
-      </div>
-      {loading ? (
-        <Loader />
       ) : (
-        <div className="container mx-auto">
-          {active === "all" && (
-            <AllUserDataTable
-              columns={allUserColumns}
-              data={allUsersData.map((user) => ({
-                id: user.id,
-                name: user.firstName + " " + user.lastName,
-                email: user.email,
-                role: user.role,
-              }))}
-            />
+        <>
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <Button
+                variant={active === "all" ? "default" : "ghost"}
+                onClick={() => {
+                  setActive("all");
+                }}
+              >
+                All
+              </Button>
+              <Button
+                variant={active === "students" ? "default" : "ghost"}
+                onClick={() => {
+                  setActive("students");
+                }}
+              >
+                Students
+              </Button>
+              <Button
+                variant={active === "instructors" ? "default" : "ghost"}
+                onClick={() => {
+                  setActive("instructors");
+                }}
+              >
+                Instructors
+              </Button>
+              <Button
+                variant={active === "admins" ? "default" : "ghost"}
+                onClick={() => {
+                  setActive("admins");
+                }}
+              >
+                Admins
+              </Button>
+            </div>
+            {active === "all" ? (
+              <Dialog>
+                <DialogTrigger asChild>
+                  <Button>
+                    <Plus /> Add User
+                  </Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-[425px]">
+                  <DialogHeader>
+                    <DialogTitle>Select User</DialogTitle>
+                    <DialogDescription>
+                      Please select the user you want to register.
+                    </DialogDescription>
+                  </DialogHeader>
+                  <div className="grid gap-4 py-4">
+                    <Button
+                      variant="outline"
+                      className="hover:bg-primary hover:text-white"
+                      onClick={() => {
+                        router.push("/admin/user-management/add-student");
+                      }}
+                    >
+                      Student
+                    </Button>{" "}
+                    <Button
+                      variant="outline"
+                      className="hover:bg-primary hover:text-white"
+                      onClick={() => {
+                        router.push("/admin/user-management/add-instructor");
+                      }}
+                    >
+                      Instructor
+                    </Button>
+                    <Button
+                      variant="outline"
+                      className="hover:bg-primary hover:text-white"
+                      onClick={() => {
+                        router.push("/admin/user-management/add-admin");
+                      }}
+                    >
+                      Admin
+                    </Button>
+                  </div>
+                </DialogContent>
+              </Dialog>
+            ) : (
+              <Button
+                onClick={() => {
+                  if (active === "admins") {
+                    router.push("/admin/user-management/add-admin");
+                  }
+
+                  if (active === "instructors") {
+                    router.push("/admin/user-management/add-instructor");
+                  }
+                  if (active === "students") {
+                    router.push("/admin/user-management/add-student");
+                  }
+                }}
+              >
+                <Plus />
+                Add
+                {active === "students" && " Student"}
+                {active === "instructors" && " Instructor"}
+                {active === "admins" && " Admin"}
+              </Button>
+            )}
+          </div>
+
+          {loading ? (
+            <Loader />
+          ) : (
+            <div className="container mx-auto">
+              {active === "all" && (
+                <AllUserDataTable
+                  columns={allUserColumns}
+                  data={allUsersData.map((user) => ({
+                    id: user.id,
+                    name: user.firstName + " " + user.lastName,
+                    email: user.email,
+                    role: user.role,
+                  }))}
+                />
+              )}
+              {active === "students" && (
+                <StudentsDataTable
+                  columns={studentsColumns}
+                  data={studentsData.map((user) => ({
+                    id: user.id,
+                    serialNumber: user.student.serialNumber,
+                    name:
+                      user.firstName +
+                      " " +
+                      user.middleInitial +
+                      " " +
+                      user.lastName,
+                    email: user.email,
+                    trainingPeriod: user.student.trainingPeriod,
+                    trainingYear: user.student.trainingYear,
+                    rank: user.student.rank,
+                    afos: user.student.afos,
+                    course: user.student.course,
+                  }))}
+                />
+              )}
+              {active === "instructors" && (
+                <InstructorsDataTable
+                  columns={instructorsColumns}
+                  data={allUsersData
+                    .filter((user) => user.role === "INSTRUCTOR")
+                    .map((user) => ({
+                      id: user.id,
+                      name: user.firstName + " " + user.lastName,
+                      email: user.email,
+                      assignedSubject: "",
+                    }))}
+                />
+              )}
+              {active === "admins" && (
+                <AdminsDataTable
+                  columns={adminsColumns}
+                  data={allUsersData
+                    .filter((user) => user.role === "ADMIN")
+                    .map((user) => ({
+                      id: user.id,
+                      name: user.firstName + " " + user.lastName,
+                      email: user.email,
+                    }))}
+                />
+              )}
+            </div>
           )}
-          {active === "students" && (
-            <StudentsDataTable
-              columns={studentsColumns}
-              data={studentsData.map((user) => ({
-                id: user.id,
-                serialNumber: user.student.serialNumber,
-                name:
-                  user.firstName +
-                  " " +
-                  user.middleInitial +
-                  " " +
-                  user.lastName,
-                email: user.email,
-                trainingPeriod: user.student.trainingPeriod,
-                trainingYear: user.student.trainingYear,
-                rank: user.student.rank,
-                afos: user.student.afos,
-                course: user.student.course,
-              }))}
-            />
-          )}
-          {active === "instructors" && (
-            <InstructorsDataTable
-              columns={instructorsColumns}
-              data={allUsersData
-                .filter((user) => user.role === "INSTRUCTOR")
-                .map((user) => ({
-                  id: user.id,
-                  name: user.firstName + " " + user.lastName,
-                  email: user.email,
-                  assignedSubject: "",
-                }))}
-            />
-          )}
-          {active === "admins" && (
-            <AdminsDataTable
-              columns={adminsColumns}
-              data={allUsersData
-                .filter((user) => user.role === "ADMIN")
-                .map((user) => ({
-                  id: user.id,
-                  name: user.firstName + " " + user.lastName,
-                  email: user.email,
-                }))}
-            />
-          )}
-        </div>
+        </>
       )}
-    </div>
+    </>
   );
 }
