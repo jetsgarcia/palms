25ee31d6e0b5012b59cf7674ad39d6c61dfdbf33
Jetter <jlgarcia2@student.apc.edu.ph>
2025-06-27 14:22:54 +0800

    feat: implement fetchAFOSDetails action and integrate it into ModulesAndSubjectsPage

---
+++ src/app/(routes)/(roles)/admin/course-management/[afosCode]/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import { fetchAFOSDetails } from "@/actions/fetchAFOSDetails";
+import ErrorMessage from "@/components/errorMessage";
+import { AFOSType } from "@/types/afos";
+import { useEffect, useState, use } from "react";
+
+export default function ModulesAndSubjectsPage({
+  params,
+}: {
+  params: Promise<{ afosCode: string }>;
+}) {
+  const { afosCode } = use(params);
+  const [afosDetails, setAFOSDetails] = useState<AFOSType>();
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function loadAFOSDetails(afosCode: string) {
+      try {
+        const response = await fetchAFOSDetails({ afosCode });
+
+        if (response.ok) {
+          setAFOSDetails({
+            code: response.data.code,
+            name: response.data.name,
+            level: response.data.level,
+            trainingPeriodId: response.data.trainingPeriodId,
+          });
+        } else {
+          setError(response.message);
+        }
+      } catch (error) {
+        console.error("Error loading AFOS details:", error);
+        setError("Failed to load AFOS details. Please try again later.");
+      }
+    }
+
+    loadAFOSDetails(afosCode);
+  }, [afosCode]);
+
+  return (
+    <>
+      {error ? <ErrorMessage error={error} /> : <div>{afosDetails?.name}</div>}
+    </>
+  );
+}
