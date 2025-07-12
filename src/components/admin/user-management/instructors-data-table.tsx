4634bae014d9a4518676bc57c5b83c720cb1cfb8
Jetter <jlgarcia2@student.apc.edu.ph>
2025-07-12 10:37:59 +0800

    refactor: update filter input to use last name instead of name in data tables

--- src/components/admin/user-management/instructors-data-table.tsx
+++ src/components/admin/user-management/instructors-data-table.tsx
@@ -56,10 +56,12 @@ export function InstructorsDataTable<TData, TValue>({
     <div>
       <div className="flex items-center py-4 gap-2">
         <Input
-          placeholder="Filter name..."
-          value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+          placeholder="Filter last name..."
+          value={
+            (table.getColumn("lastName")?.getFilterValue() as string) ?? ""
+          }
           onChange={(event) =>
-            table.getColumn("name")?.setFilterValue(event.target.value)
+            table.getColumn("lastName")?.setFilterValue(event.target.value)
           }
         />
         <Input
