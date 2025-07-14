47159062c3e0a7d917c97c719babf6cdc0a2dbe7
Jetter <jlgarcia2@student.apc.edu.ph>
2025-07-14 14:58:53 +0800

    fix: standardize error handling in course form schema

--- src/schemas/courseForm.ts
+++ src/schemas/courseForm.ts
@@ -2,12 +2,14 @@ import { Level } from "@prisma/client";
 import { z } from "zod";
 
 export const courseFormSchema = z.object({
-  code: z.string().min(1, { message: "Code is required" }),
-  name: z.string().min(1, { message: "Name is required" }),
+  code: z
+    .string({ required_error: "Code is required" })
+    .min(1, "Code is required"),
+  name: z
+    .string({ required_error: "Name is required" })
+    .min(1, "Name is required"),
   level: z.nativeEnum(Level, {
     errorMap: () => ({ message: "Level is required" }),
   }),
-  trainingPeriodId: z
-    .number()
-    .min(1, { message: "Training Period is required" }),
+  trainingPeriodId: z.number({ required_error: "Training Period is required" }),
 });
