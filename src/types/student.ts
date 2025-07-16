a58730ad8ffb235c3e8273394dbf6239e8f7bf49
Jetter <jlgarcia2@student.apc.edu.ph>
2025-07-16 12:33:07 +0800

    feat: enhance StudentType to include course and training period relations

--- src/types/student.ts
+++ src/types/student.ts
@@ -1,5 +1,9 @@
-import { students, users } from "@prisma/client";
+import { courses, training_periods, students, users } from "@prisma/client";
 
 export interface StudentType extends users {
-  student: students;
+  student: students & {
+    courses: courses & {
+      training_periods: training_periods;
+    };
+  };
 }
