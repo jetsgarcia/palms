fbfa7493f030b29dd30a681a9d1781449512557a
Jetter <jlgarcia2@student.apc.edu.ph>
2025-07-16 15:04:31 +0800

    feat: refactor UserManagementPage to use dropdown menu for tab selection

--- src/app/(routes)/(roles)/admin/user-management/page.tsx
+++ src/app/(routes)/(roles)/admin/user-management/page.tsx
@@ -2,36 +2,60 @@
 
 import StudentTab from "@/components/admin/user-management/student-tab";
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ChevronDown, Filter, Plus } from "lucide-react";
 import { useState } from "react";
 
 export default function UserManagementPage() {
   const [selectedTab, setSelectedTab] = useState<
-    "Students" | "Instructors" | "Admin"
-  >("Students");
+    "Student" | "Instructor" | "Admin"
+  >("Student");
 
   return (
     <div className="space-y-4">
-      <div className="tabs">
-        <Button
-          variant={selectedTab === "Students" ? "default" : "ghost"}
-          onClick={() => setSelectedTab("Students")}
-        >
-          Students
-        </Button>
-        <Button
-          variant={selectedTab === "Instructors" ? "default" : "ghost"}
-          onClick={() => setSelectedTab("Instructors")}
-        >
-          Instructors
-        </Button>
-        <Button
-          variant={selectedTab === "Admin" ? "default" : "ghost"}
-          onClick={() => setSelectedTab("Admin")}
-        >
-          Admin
+      {/* Header */}
+      <div className="flex items-center justify-between">
+        <h1 className="text-3xl font-bold">Users</h1>
+        <div>
+          <Button size="lg">
+            <Plus /> Add {selectedTab.toLowerCase()}
+          </Button>
+        </div>
+      </div>
+      <div className="flex items-center justify-between">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button
+              variant="outline"
+              className="w-[8rem] flex items-center justify-between"
+            >
+              {selectedTab} <ChevronDown />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuItem onClick={() => setSelectedTab("Student")}>
+              Students
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setSelectedTab("Instructor")}>
+              Instructors
+            </DropdownMenuItem>
+            <DropdownMenuItem onClick={() => setSelectedTab("Admin")}>
+              Admin
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+        <Button variant="ghost" size="sm" className="text-sm w-fit">
+          <Filter /> Filter
         </Button>
       </div>
-      {selectedTab === "Students" && <StudentTab />}
+
+      {/* Main content */}
+      {selectedTab === "Student" && <StudentTab />}
     </div>
   );
 }
