41f7013a0c4d537508387788225599955fc174ee
Jetter <jlgarcia2@student.apc.edu.ph>
2025-07-17 11:44:56 +0800

    feat: implement validation schema for student registration form

--- src/app/(routes)/(roles)/admin/user-management/page.tsx
+++ src/app/(routes)/(roles)/admin/user-management/page.tsx
@@ -1,17 +1,20 @@
 "use client";
 
+import { useState } from "react";
+import { ChevronDown, Filter, Plus } from "lucide-react";
+import { StudentForm } from "@/components/admin/user-management/student-form";
 import StudentTab from "@/components/admin/user-management/student-tab";
 import { Button } from "@/components/ui/button";
+import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { ChevronDown, Filter, Plus } from "lucide-react";
-import { useState } from "react";
 
 export default function UserManagementPage() {
+  const [openAddStudentDialog, setOpenAddStudentDialog] = useState(false);
   const [selectedTab, setSelectedTab] = useState<
     "Student" | "Instructor" | "Admin"
   >("Student");
@@ -22,9 +25,17 @@ export default function UserManagementPage() {
       <div className="flex items-center justify-between">
         <h1 className="text-3xl font-bold">Users</h1>
         <div>
-          <Button size="lg">
-            <Plus /> Add {selectedTab.toLowerCase()}
-          </Button>
+          <Dialog
+            open={openAddStudentDialog}
+            onOpenChange={setOpenAddStudentDialog}
+          >
+            <DialogTrigger asChild>
+              <Button size="lg">
+                <Plus /> Add {selectedTab.toLowerCase()}
+              </Button>
+            </DialogTrigger>
+            <StudentForm setOpenDialog={setOpenAddStudentDialog} />
+          </Dialog>
         </div>
       </div>
       <div className="flex items-center justify-between">
--- src/components/admin/user-management/student-form.tsx
+++ src/components/admin/user-management/student-form.tsx
@@ -1,10 +1,18 @@
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import z from "zod";
-import { toast } from "sonner";
+import { readCourses } from "@/actions/courses";
+import { createStudent } from "@/actions/student";
+import { Button } from "@/components/ui/button";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
 import {
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog";
@@ -17,96 +25,128 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
-import { Button } from "../../ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+import { cn } from "@/lib/utils";
 import { studentRegisterFormSchema } from "@/schemas/studentRegisterForm";
-import { createStudent } from "@/actions/student";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { courses } from "@prisma/client";
+import { Check, ChevronsUpDown, Loader2Icon } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+import { z } from "zod";
 
 interface StudentFormProps {
-  mode: "add" | "edit";
-  refreshUsers: () => void;
-  setFormOpen: (open: boolean) => void;
-  initialData?: z.infer<typeof studentRegisterFormSchema>;
-  id?: string;
+  setOpenDialog: (open: boolean) => void;
 }
 
-export default function StudentForm({
-  mode,
-  refreshUsers,
-  setFormOpen,
-  initialData,
-  id,
-}: StudentFormProps) {
+export function StudentForm({ setOpenDialog }: StudentFormProps) {
+  const [loading, setLoading] = useState(true);
   const [isSubmitting, setIsSubmitting] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [courses, setCourses] = useState<courses[]>([]);
 
   const form = useForm<z.infer<typeof studentRegisterFormSchema>>({
     resolver: zodResolver(studentRegisterFormSchema),
-    defaultValues: initialData ?? {
-      firstName: "",
-      lastName: "",
-      middleInitial: "",
-      suffix: "",
-      email: "",
+    defaultValues: {
       serialNumber: "",
+      firstName: "",
+      middleInitial: "",
+      lastName: "",
+      suffix: "",
       rank: "",
-      afos: "",
-      course: "",
+      email: "",
     },
   });
 
   async function onSubmit(values: z.infer<typeof studentRegisterFormSchema>) {
     setIsSubmitting(true);
+
     try {
-      let response;
+      const response = await createStudent(values);
 
-      if (mode === "edit" && id) {
-        // response = await updateStudent(values, id);
-      } else if (mode === "add") {
-        response = await createStudent(values);
-      }
-
-      if (response && response.ok) {
-        toast.success(
-          `Student ${mode === "add" ? "registered" : "updated"} successfully`
-        );
-        setFormOpen(false);
-        refreshUsers();
-        setIsSubmitting(false);
+      if (response.ok) {
+        toast.success("Student registered successfully");
         form.reset();
-      } else if (response && !response.ok) {
+        setIsSubmitting(false);
+        setOpenDialog(false);
+      } else {
         toast.error(response.message);
         setIsSubmitting(false);
       }
     } catch (error) {
-      console.error(error);
-      toast.error(`Failed to ${mode === "add" ? "register" : "edit"} student.`);
+      console.error("Error creating student:", error);
+      toast.error("An error occurred while registering the student");
       setIsSubmitting(false);
     }
   }
 
+  useEffect(() => {
+    async function fetchCourses() {
+      try {
+        const response = await readCourses();
+        if (response.ok) {
+          setCourses(response.data);
+          setLoading(false);
+        } else {
+          console.log("Error fetching courses:", response.message);
+          toast.error("Failed to fetch courses");
+          setLoading(false);
+        }
+      } catch (error) {
+        console.error("Error fetching courses:", error);
+        toast.error("An error occurred while fetching courses");
+        setLoading(false);
+      }
+    }
+    fetchCourses();
+  }, []);
+
+  const courseRef = useRef<HTMLButtonElement>(null);
+
   return (
-    <DialogContent className="min-w-min">
+    <DialogContent>
       <DialogHeader>
-        <DialogTitle>
-          {mode === "add" ? "Register" : "Edit"} student
-        </DialogTitle>
+        <DialogTitle>Register student</DialogTitle>
         <DialogDescription>
-          {mode === "add"
-            ? "Fill in the details below to register a student."
-            : "Edit the details below for this student."}
+          Fill out the form below to register a new student
         </DialogDescription>
       </DialogHeader>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-          <div className="flex flex-col md:flex-row gap-4">
-            <div className="flex-1">
+      {loading ? (
+        <div className="flex items-center justify-center h-64">
+          <Loader2Icon size={48} color="darkGreen" className="animate-spin" />
+        </div>
+      ) : (
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+            {/* First row*/}
+            <div className="flex gap-4">
               <FormField
                 control={form.control}
                 name="serialNumber"
                 render={({ field }) => (
-                  <FormItem>
+                  <FormItem className="flex-1">
                     <FormLabel>
-                      Serial number <span className="text-destructive">*</span>
+                      Serial number<span className="text-destructive">*</span>
+                    </FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="rank"
+                render={({ field }) => (
+                  <FormItem className="flex-1">
+                    <FormLabel>
+                      Rank<span className="text-destructive">*</span>
                     </FormLabel>
                     <FormControl>
                       <Input {...field} />
@@ -116,12 +156,61 @@ export default function StudentForm({
                 )}
               />
             </div>
-            <div className="flex-1">
+
+            {/* Second row*/}
+            <div className="flex gap-4">
+              <FormField
+                control={form.control}
+                name="firstName"
+                render={({ field }) => (
+                  <FormItem className="flex-3">
+                    <FormLabel>
+                      First name<span className="text-destructive">*</span>
+                    </FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="middleInitial"
+                render={({ field }) => (
+                  <FormItem className="flex-1">
+                    <FormLabel>Middle initial</FormLabel>
+                    <FormControl>
+                      <Input {...field} className="uppercase" maxLength={1} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            {/* Third row*/}
+            <div className="flex gap-4">
+              <FormField
+                control={form.control}
+                name="lastName"
+                render={({ field }) => (
+                  <FormItem className="flex-3">
+                    <FormLabel>
+                      Last name<span className="text-destructive">*</span>
+                    </FormLabel>
+                    <FormControl>
+                      <Input {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
               <FormField
                 control={form.control}
                 name="suffix"
                 render={({ field }) => (
-                  <FormItem>
+                  <FormItem className="flex-1">
                     <FormLabel>Suffix</FormLabel>
                     <FormControl>
                       <Input {...field} />
@@ -131,183 +220,120 @@ export default function StudentForm({
                 )}
               />
             </div>
-          </div>
 
-          <div className="flex flex-col md:flex-row gap-4">
-            <div className="flex-1">
-              <FormField
-                control={form.control}
-                name="firstName"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>
-                      First name <span className="text-destructive">*</span>
-                    </FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-            <div className="w-32">
-              <FormField
-                control={form.control}
-                name="middleInitial"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Middle initial</FormLabel>
-                    <FormControl>
-                      <Input
-                        {...field}
-                        maxLength={1}
-                        onChange={(e) => {
-                          const value = e.target.value
-                            .replace(/[^a-zA-Z]/g, "")
-                            .toUpperCase()
-                            .slice(0, 1);
-                          field.onChange(value);
-                        }}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-            <div className="flex-1">
-              <FormField
-                control={form.control}
-                name="lastName"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>
-                      Last name <span className="text-destructive">*</span>
-                    </FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-          </div>
+            {/* Fourth row */}
+            <FormField
+              control={form.control}
+              name="email"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>
+                    Email
+                    <span className="text-destructive">*</span>
+                  </FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="email"
+                      onKeyDown={(e) => {
+                        if (e.key === "Tab" && !e.shiftKey) {
+                          e.preventDefault();
+                          courseRef.current?.focus();
+                        }
+                      }}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
 
-          <div className="flex flex-col md:flex-row gap-4">
-            <div className="flex-1">
-              <FormField
-                control={form.control}
-                name="trainingPeriod"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>
-                      Training Period{" "}
-                      <span className="text-destructive">*</span>
-                    </FormLabel>
-                    <FormControl>
-                      <Input
-                        {...field}
-                        id="trainingPeriod"
-                        name="trainingPeriod"
-                        onChange={(e) => {
-                          const value = e.target.value;
-                          if (/^\d*$/.test(value)) {
-                            field.onChange(
-                              value === "" ? undefined : Number(value)
-                            );
-                          }
-                        }}
-                        value={field.value ?? ""}
-                      />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-            <div className="flex-1">
-              <FormField
-                control={form.control}
-                name="rank"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>
-                      Rank <span className="text-destructive">*</span>
-                    </FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-          </div>
-
-          <div className="flex flex-col md:flex-row gap-4">
-            <div className="flex-1">
-              <FormField
-                control={form.control}
-                name="afos"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>
-                      AFOS <span className="text-destructive">*</span>
-                    </FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-            <div className="flex-1">
-              <FormField
-                control={form.control}
-                name="course"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>
-                      Course <span className="text-destructive">*</span>
-                    </FormLabel>
-                    <FormControl>
-                      <Input {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </div>
-          </div>
-
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>
-                  Email <span className="text-destructive">*</span>
-                </FormLabel>
-                <FormControl>
-                  <Input {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-
-          <div className="flex items-center justify-end space-x-4">
-            <div className="flex items-center space-x-4">
+            {/* Fifth row */}
+            <FormField
+              control={form.control}
+              name="course"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Course</FormLabel>
+                  <FormControl>
+                    <Popover open={open} onOpenChange={setOpen}>
+                      <PopoverTrigger asChild>
+                        <Button
+                          ref={courseRef}
+                          variant="outline"
+                          role="combobox"
+                          aria-expanded={open}
+                          className={cn(
+                            "w-full justify-between",
+                            form.formState.errors.course
+                              ? "border-destructive focus-visible:ring-destructive"
+                              : "border-primary",
+                            !field.value && "text-gray-500 hover:text-gray-500"
+                          )}
+                        >
+                          {courses.length > 0
+                            ? (() => {
+                                const selected = courses.find(
+                                  (course) => course.code === field.value
+                                );
+                                if (!selected) return "Select a course";
+                                return selected.code;
+                              })()
+                            : "Select a course"}
+                          <ChevronsUpDown className="opacity-50" />
+                        </Button>
+                      </PopoverTrigger>
+                      <PopoverContent className="w-[29rem] p-0">
+                        <Command>
+                          <CommandInput
+                            className="h-9"
+                            placeholder="Search course..."
+                          />
+                          <CommandList>
+                            <CommandEmpty>No course found</CommandEmpty>
+                            <CommandGroup>
+                              {courses.map((course) => {
+                                return (
+                                  <CommandItem
+                                    key={course.code}
+                                    value={course.code}
+                                    onSelect={() => {
+                                      if (field.value !== course.code) {
+                                        field.onChange(course.code);
+                                      }
+                                      setOpen(false);
+                                    }}
+                                  >
+                                    {course.code}
+                                    <Check
+                                      className={cn(
+                                        "ml-auto",
+                                        field.value === course.code
+                                          ? "opacity-100"
+                                          : "opacity-0"
+                                      )}
+                                    />
+                                  </CommandItem>
+                                );
+                              })}
+                            </CommandGroup>
+                          </CommandList>
+                        </Command>
+                      </PopoverContent>
+                    </Popover>
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter>
               <Button type="submit" disabled={isSubmitting}>
                 Submit
               </Button>
-            </div>
-          </div>
-        </form>
-      </Form>
+            </DialogFooter>
+          </form>
+        </Form>
+      )}
     </DialogContent>
   );
 }
